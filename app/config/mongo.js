a36737c7ae9db9b949faa0239baf0adba9081b44
Sebastian Sossa <s_o_s_a_1999@hotmail.com>
2021-12-05 15:29:55 -0500

    Project ready for production

--- app/config/mongo.js
+++ app/config/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDatabase = () => {
   const options = { useNewUrlParser: true, useUnifiedTopology: true }
-  mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`, options)
+  mongoose.connect(process.env.DB_URL, options)
     .then(() => console.log('Database is connected'))
     .catch(error => console.error(error))
 }
--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const express = require('express')
 const cors = require('cors')
 const connectDatabase = require('./app/config/mongo')
 const app = express()
-const PORT = process.env.PORT || 3000
 
 // Settings
 connectDatabase()
 app.use(express.static(__dirname + '/public'))
-app.use(cors())
+app.use(cors({ origin: '*', optionsSuccessStatus: 200 }))
 app.use(express.json())
 
 // Routes
@@ -17,6 +16,8 @@ app.use('/api/products', verifySession, require('./app/routes/products'))
 app.use('/api/auth', require('./app/routes/auth'))
 
 // Server port
-app.listen(PORT, () => {
+const HOST = process.env.HOST || '0.0.0.0'
+const PORT = process.env.PORT || 3000
+app.listen(PORT, HOST, () => {
   console.log('Server listen on port', PORT)
 })
